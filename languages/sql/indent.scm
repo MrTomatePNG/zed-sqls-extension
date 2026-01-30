0947a7254a5b2e8637cd6ddfcf6cc683a0a0f411
MrTomatePNG <mrtomate360@outlook.com>
2026-01-30 17:43:39 -0400

    Add SQL language support
    Includes bracket matching, indentation rules, outline capture, and
    runnable detection for SQL statements.

---
+++ languages/sql/indent.scm
@@ -0,0 +1,3 @@
+(select_statement ")" @end) @indent
+(create_statement ")" @end) @indent
+(if_statement "end" @end) @indent
---
+++ languages/sql/outline.scm
@@ -0,0 +1,11 @@
+; Table definitions
+(create_table_statement
+  name: (identifier) @name) @item
+
+; Function definitions
+(create_function_statement
+  name: (identifier) @name) @item
+
+; View definitions
+(create_view_statement
+  name: (identifier) @name) @item
