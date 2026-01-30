7fdb7fbe69480398586b5dab54dc4c560ef0e5ce
MrTomatePNG <mrtomate360@outlook.com>
2026-01-30 17:43:53 -0400

    Add syntax highlighting for SQL

--- languages/sql/highlights.scm
+++ languages/sql/highlights.scm
@@ -1,14 +1,30 @@
 ; Keywords
 (keyword) @keyword
 
-; Comments
-(comment) @comment
-
 ; Strings
 (string) @string
+(string_escape) @string.escape
 
 ; Numbers
 (number) @number
 
-; Identifiers/Variables
+; Comments
+(comment) @comment
+(comment_doc) @comment.doc
+
+; Operators
+(operator) @operator
+
+; Punctuation
+(punctuation) @punctuation
+(punctuation_bracket) @punctuation.bracket
+(punctuation_delimiter) @punctuation.delimiter
+
+; Identifiers
 (identifier) @variable
+
+; Built-in functions
+(builtin_function) @function.builtin
+
+; Types
+(type) @type
